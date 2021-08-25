aadb3115527327cc2fd0741ccc30e2ef388c5e5f
Zoe Hart <zoerhart@gmail.com>
2021-08-25 14:01:35 +1200

    js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('nav-links'); 
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-    });
-}
+   burger.addEventListener('click',()=>{
+    nav.classList.toggle('nav-active');
+   })
+   }
 
-navSlide();
+navSlide(); 
