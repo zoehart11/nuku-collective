70a69ad0a479a9767dd4244c76ecac5994e29adb
Zoe Hart <zoerhart@gmail.com>
2021-11-16 10:05:10 +1300

    d

---
+++ contactengine.php
@@ -0,0 +1,44 @@
+#!/usr/local/php70/bin/php-cli -q
+<?php
+
+$EmailFrom = "studentmail@design.ac.nz";
+$EmailTo = "zoehartdesign@gmail.com";
+$Subject = "Contact form from Nuku Collective";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Tel = Trim(stripslashes($_POST['Tel'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contact-error.html\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contact-thanks.html\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contact-error.html\">";
+}
+?>
